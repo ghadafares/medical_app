b63fc20618a77a244936d4875f8cd8f162c4b232
ghadafares <71799755+ghadafares@users.noreply.github.com>
2022-07-11 03:22:27 +0200

    first commit

---
+++ lib/appOpening/splash_screen.dart
@@ -0,0 +1,26 @@
+
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+import 'login_screens/login.dart';
+
+class Splash extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EasySplashScreen(
+        backgroundColor:backgrouColor ,
+        logo: Image.asset('assets/images/splash.png'),
+        logoSize: 300,
+        //title: Text('MedLife',style: TextStyle(color: KMainColor,fontSize: 25),),
+        navigator: Login(),
+        showLoader: true,
+        loadingText: Text("Loading...",style: TextStyle(color: Colors.white),),
+        durationInSeconds: 7,
+      ),
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+// @dart=2.9
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_profile_shared_preferences_example/Bloc/AppCubit/cubit.dart';
+import 'package:user_profile_shared_preferences_example/Bloc/AppStates/states.dart';
+import 'package:user_profile_shared_preferences_example/appOpening/splash_screen.dart';
+import 'package:user_profile_shared_preferences_example/navigationBarPages/home_page.dart';
+import 'package:user_profile_shared_preferences_example/page/edit_profile_page.dart';
+import 'package:user_profile_shared_preferences_example/page/profile_page.dart';
+import 'package:user_profile_shared_preferences_example/themes.dart';
+import 'package:user_profile_shared_preferences_example/utils/user_preferences.dart';
+
+import 'add_new_medicine/add_new_medicine.dart';
+import 'constants/colors.dart';
+import 'home/alarm_page.dart';
+import 'navigationBarPages/first_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  await UserPreferences.init();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'User Profile';
+
+  @override
+  Widget build(BuildContext context) {
+    final user = UserPreferences.getUser();
+
+    return BlocProvider(
+
+      create: (BuildContext context) =>AppCubit()..getProfile(context)..getUserDiseases(),
+      child: BlocConsumer<AppCubit,AppStates>(
+        listener: (context,state){
+
+        },
+        builder: (context,state){
+          return ThemeProvider(
+
+            initTheme: user.isDarkMode! ? MyThemes.darkTheme : MyThemes.lightTheme,
+            child: Builder(
+              builder: (context) => MaterialApp(
+                  theme: ThemeData(
+                      fontFamily: "Popins",
+                      //primaryColor: Color.fromRGBO(7, 190, 200, 1),
+                      primaryColor: KMainColor,
+                      textTheme: TextTheme(
+                          headline1: ThemeData.light().textTheme.headline1!.copyWith(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 38.0,
+                            fontFamily: "Popins",
+                          ),
+                          headline5: ThemeData.light().textTheme.headline1!.copyWith(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 17.0,
+                            fontFamily: "Popins",
+                          ),
+                          headline3: ThemeData.light().textTheme.headline3!.copyWith(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20.0,
+                            fontFamily: "Popins",
+                          ))),
+                  routes: {
+                    "/": (context) => Splash(),
+                    "/home": (context) => Alarm(),
+                    "/add_new_medicine": (context) => AddNewMedicine(),
+                  },
+                  initialRoute: "/",
+                  debugShowCheckedModeBanner: false,
+                  title: title,
+
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
